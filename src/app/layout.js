fed1d0ada7d580b857633f0b85b120fd9356f1c7
Airteg <airteg@ukr.net>
2025-04-12 12:58:53 +0300

    styles-styles-styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         <AuthProvider>
           <div className="flex flex-col min-h-screen min-w-screen">
             <Menu />
-            <main className="min-w-full flex flex-1  mx-auto p-0">
+            <main className="min-w-full flex flex-1 justify-center mx-auto p-0">
               {children}
             </main>
           </div>
